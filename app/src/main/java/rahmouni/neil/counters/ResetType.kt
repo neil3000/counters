fc01a136ed6faabdf51ebe87f2017270206fc0df
Neïl <neil.rahmouni@gmail.com>
2022-03-28 02:13:29 +0200

    Added ResetType for week and month

--- app/src/main/java/rahmouni/neil/counters/ResetType.kt
+++ app/src/main/java/rahmouni/neil/counters/ResetType.kt
@@ -3,4 +3,6 @@ package rahmouni.neil.counters
 enum class ResetType(val title: Int, val formatted: Int) {
     NEVER(R.string.text_never, R.string.text_never_resets),
     DAY(R.string.text_everyDay, R.string.text_resetsEveryDayToX),
+    WEEK(R.string.text_everyWeek, R.string.text_resetsEveryWeekToX),
+    MONTH(R.string.text_everyMonth, R.string.text_resetsEveryMonthToX),
 }
