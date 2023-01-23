d0b8dca32f3a530995ae9b4acb11617d0e74f77d
neil <neil.rahmouni@gmail.com>
2023-01-23 22:28:31 +0100

    Removed unused imports

--- app/src/main/java/rahmouni/neil/counters/counterActivity/CounterActivity.kt
+++ app/src/main/java/rahmouni/neil/counters/counterActivity/CounterActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
