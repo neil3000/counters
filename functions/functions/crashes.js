73f216a14fa734d8b34405e19d95e0347ecaf242
Neïl Rahmouni <neil.rahmouni@gmail.com>
2024-05-30 23:29:00 +0200

    Fixed #388

--- functions/functions/crashes.js
+++ functions/functions/crashes.js
@@ -25,7 +25,7 @@ exports.crashes = onNewFatalIssuePublished(async (event) => {
   try {
     // STEP ONE -- Create a Gitlab issue
     const gitlabIssue = await fetch(
-      `https://gitlab.com/api/v4/projects/${gitlabProjectId}/issues?title=Invstigate%20fatal%20issue&labels=bug,critical,Planned&description=https://counters.rahmouni.dev/report/${crashlyticsId}`,
+      `https://gitlab.com/api/v4/projects/${gitlabProjectId}/issues?title=Invstigate%20fatal%20issue&labels=bug,critical,Planned&description=https://counters.rahmouni.dev/report/${event.appId.includes("demo")?"demo":"prod"}/${crashlyticsId}`,
       {
         method: "POST",
         headers: { "PRIVATE-TOKEN": gitlabProjectPrivateToken },
@@ -59,7 +59,7 @@ exports.crashes = onNewFatalIssuePublished(async (event) => {
                 },
                 {
                   name: "<:firebase:1245740009730871318>  Report & logs",
-                  value: `[counters.rahmouni.dev/report/${crashlyticsId}](https://counters.rahmouni.dev/report/${crashlyticsId})`,
+                  value: `[counters.rahmouni.dev/report/${event.appId.includes("demo")?"demo":"prod"}/${crashlyticsId}](https://counters.rahmouni.dev/report/${event.appId.includes("demo")?"demo":"prod"}/${crashlyticsId})`,
                 },
               ],
             },
