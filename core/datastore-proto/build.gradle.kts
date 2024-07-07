cf5e498e64d22054dccf2e195a9b84111a91b461
Neïl Rahmouni <neil.rahmouni@gmail.com>
2024-07-08 01:58:51 +0200

    Added LICENSE headers to files & ran spotless

--- core/datastore-proto/build.gradle.kts
+++ core/datastore-proto/build.gradle.kts
@@ -1,6 +1,5 @@
 /*
- * Counters
- * Copyright (C) $year Rahmouni Neïl
+ * Copyright (C) 2024 Rahmouni Neïl
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -11,10 +10,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *c
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 plugins {
     alias(libs.plugins.rn3.android.library)
     alias(libs.plugins.protobuf)
--- spotless/copyright.kts
+++ spotless/copyright.kts
@@ -1,6 +1,5 @@
 /*
- * Counters
- * Copyright (C) $year Rahmouni Neïl
+ * Copyright (C) $YEAR Rahmouni Neïl
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -11,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *c
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
