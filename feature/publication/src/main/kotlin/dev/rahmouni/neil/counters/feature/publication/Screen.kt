e4dfb4f66bcaaed82c576af7694adcaf1ad45f80
Laizo <aureliencoppee@gmail.com>
2024-07-19 23:56:04 +0200

    Start publish page

--- feature/publication/src/main/kotlin/dev/rahmouni/neil/counters/feature/publication/Screen.kt
+++ feature/publication/src/main/kotlin/dev/rahmouni/neil/counters/feature/publication/Screen.kt
@@ -18,15 +18,46 @@
 package dev.rahmouni.neil.counters.feature.publication
 
 import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddPhotoAlternate
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.rahmouni.neil.counters.core.designsystem.TopAppBarAction
-import dev.rahmouni.neil.counters.core.designsystem.component.Rn3Scaffold
-import dev.rahmouni.neil.counters.core.designsystem.component.TopAppBarStyle.SMALL
+import dev.rahmouni.neil.counters.core.designsystem.component.Rn3IconButton
+import dev.rahmouni.neil.counters.core.designsystem.component.topAppBar.Rn3SmallTopAppBar
 import dev.rahmouni.neil.counters.core.feedback.FeedbackContext.FeedbackScreenContext
 import dev.rahmouni.neil.counters.core.feedback.navigateToFeedback
 import dev.rahmouni.neil.counters.core.ui.TrackScreenViewEvent
@@ -52,6 +83,7 @@ internal fun PublicationRoute(
     TrackScreenViewEvent(screenName = "Publication")
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
 @Composable
 internal fun PublicationScreen(
@@ -60,19 +92,90 @@ internal fun PublicationScreen(
     feedbackTopAppBarAction: TopAppBarAction? = null,
     onSettingsTopAppBarActionClicked: () -> Unit = {},
 ) {
-    Rn3Scaffold(
-        modifier = modifier,
-        topAppBarTitle = stringResource(string.feature_publication_topAppBarTitle),
-        onBackIconButtonClicked = onBackIconButtonClicked,
-        topAppBarActions = listOfNotNull(
-            TopAppBarAction(
-                Icons.Outlined.Settings,
-                stringResource(string.feature_publication_topAppBarActions_settings),
-                onSettingsTopAppBarActionClicked,
-            ),
-            feedbackTopAppBarAction,
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        SheetState(
+            initialValue = SheetValue.Expanded,
+            density = LocalDensity.current,
+            skipPartiallyExpanded = true,
         ),
-        topAppBarStyle = SMALL,
-    ) {
+    )
+
+    val focusRequester = remember { FocusRequester() }
+
+    var currentDescription by rememberSaveable { mutableStateOf("") }
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        topBar = {
+            Rn3SmallTopAppBar(
+                title = {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.CenterHorizontally),
+                        text = stringResource(string.feature_publication_topAppBarTitle),
+                    )
+                },
+                scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+                onBackIconButtonClicked = onBackIconButtonClicked,
+                actions = listOfNotNull(
+                    feedbackTopAppBarAction,
+                    TopAppBarAction(
+                        Icons.Outlined.Settings,
+                        stringResource(string.feature_publication_topAppBarActions_settings),
+                        onSettingsTopAppBarActionClicked,
+                    ),
+                ),
+            )
+        },
+        scaffoldState = scaffoldState,
+        sheetDragHandle = null,
+        sheetSwipeEnabled = false,
+        sheetShape = RoundedCornerShape(0.dp),
+        sheetContent = {
+            Column {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(start = 0.dp, end = 8.dp, top = 4.dp, bottom = 4.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Rn3IconButton(
+                        icon = Icons.Outlined.AddPhotoAlternate,
+                        contentDescription = "import image",
+                        onClick = {},
+                    )
+                    Button(
+                        onClick = { },
+                    ) {
+                        Text("Post")
+                    }
+                }
+                Spacer(Modifier.imePadding())
+            }
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+        ) {
+            OutlinedTextField(
+                value = currentDescription,
+                onValueChange = { currentDescription = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .focusRequester(focusRequester),
+                label = { Text(text = stringResource(string.feature_publication_textField_label)) },
+                singleLine = false,
+                keyboardActions = KeyboardActions(
+                    onDone = {
+
+                    },
+                ),
+            )
+
+        }
     }
 }
